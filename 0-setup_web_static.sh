562a1d5001f14e608c94e27ce7e9fa84e80ce56b
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-12-13 15:25:36 +0100

    Prepare webservers for static content deployment.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # Bash script that sets up your web servers for the deployment of web_static.
 sudo apt update -y
 sudo apt install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
 
 echo "<!DOCTYPE html>
 <html>
@@ -14,7 +14,7 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
 sudo sed -i '38 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
