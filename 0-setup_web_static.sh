7f30197fd81daf9f1d966dffb041a7edaf0cd272
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-12-13 13:31:52 +0100

    Prepare webservers for static content deployment.

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static.
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Hlberton Coding School, Alx
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+
+ln -s /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+
+sudo sed -i '38 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
