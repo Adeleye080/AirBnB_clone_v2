66dd3aaf37f7f96a84da0100c5ace76fc9ae1264
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-01-15 00:57:25 +0100

    made all files pycodestyle compliant.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -8,20 +8,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def Home():
     """This is the Home Page"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def HBNB():
     """Displays 'HBNB'"""
     return "HBNB"
 
+
 @app.route('/c/<text>')
 def C_is_fun(text):
     """displays 'C' followed by text added in route"""
     return "C {}".format(text)
+
 
 @app.route('/python')
 @app.route('/python/<text>')
@@ -29,14 +33,17 @@ def python_is_cool(text='is_cool'):
     """display python followed by text, replacing '_' with space"""
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 @app.route('/number/<int:n>')
 def is_number(n):
     """displays message if n is an integer"""
     return f'{n} is a number'
 
+
 @app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', number=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,15 +10,18 @@ from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def close_db_session(exception):
     storage.close()
+
 
 @app.route('/states_list')
 def get_states_list():
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
