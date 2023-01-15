d7982fc1b8e5dee0e21febeeeb745e4591d22b83
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-01-15 13:32:30 +0100

    declared sqlalchemy tables and added neccessary columns.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City',cascade="all, delete, save-update,\
+                          delete-orphan", backref='state')
+
+    @property
+    def cities(self):
+        from models import storage
+        data = storage.all()
+        results = []
+        city_instance = []
+        for key, value in data.items():
+            if value.__class__.__name__ == "City":
+                city_instance.append(data[key])
+        for city in city_instance:
+            if city.state_id == self.id:
+                results.append(city)
+        return results
